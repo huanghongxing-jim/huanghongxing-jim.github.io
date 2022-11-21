a5396930225e4e9fcdd122961456e057febde155
jim <61418362+huanghongxing-jim@users.noreply.github.com>
2022-11-21 21:07:13 +0800

    modify

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 Huang Hongxing's Blog.
 
-To Learn more, to recorder more!
+> To Learn more, to recorder more!
 
+
+**todo:**
+1. 制作工具一键添加`{% raw %}`和`{% endraw %}`到代码块上下部分，`{{ }}`和`{% %}`中。
+2. 如果使用`<name>`，build是会报错。
+
+
+
+My blog is based on `jekyll-theme-chirpy`.
 
--- _plugins/post-cmd-hhx.rb
+++ _plugins/post-cmd-hhx.rb
@@ -2,6 +2,7 @@ Jekyll::Hooks.register :documents, :post_write do
 
     # copy adaptable ico images to _site dir.
     `cp $(pwd)/assets/images/favicons/* $(pwd)/_site/assets/img/favicons/`
+    # copy attachments dir to _site dir.
     `cp -r $(pwd)/assets/attachments/ $(pwd)/_site/assets/`
 
   end
