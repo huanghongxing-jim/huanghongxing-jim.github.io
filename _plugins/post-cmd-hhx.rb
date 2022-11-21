cc0e39a4060eeaed490cccbe168015206e79a542
jim <61418362+huanghongxing-jim@users.noreply.github.com>
2022-11-21 15:22:02 +0800

    cp attachments

--- _plugins/post-cmd-hhx.rb
+++ _plugins/post-cmd-hhx.rb
@@ -2,5 +2,6 @@ Jekyll::Hooks.register :documents, :post_write do
 
     # copy adaptable ico images to _site dir.
     `cp $(pwd)/assets/images/favicons/* $(pwd)/_site/assets/img/favicons/`
+    `cp -r $(pwd)/assets/attachments/ $(pwd)/_site/assets/attachments/`
 
   end
